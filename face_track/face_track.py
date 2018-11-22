53a0820fd291bb1496cf407f94cbc4e1629ae2bc
eflorens <44293900+eflorens@users.noreply.github.com>
2018-11-22 10:31:52 +0100

    Add files via upload

--- face_track/face_track.py
+++ face_track/face_track.py
@@ -1,16 +1,18 @@
 import cv2
 import numpy as np
+import time
+import sys
 
 
 def store_faces():
-    id = 0
     for (x, y, w, h) in faces:
         print(x, y, w, h, len(faces))
         gregion = gframe[y:y+h, x:x+w]
         cregion = cframe[y:y+h, x:x+w]
-        frame_capture_name = "./faces/capture-" + str(id) + ".png"
+        global fid
+        frame_capture_name = "capture-" + str(fid) + ".png"
         cv2.imwrite(frame_capture_name, cregion)
-        id = id + 1
+        fid = fid + 1
 
         
 def show_faces():
@@ -24,8 +26,14 @@ def show_faces():
 
 cascade_frontal_face = cv2.CascadeClassifier("./cascades/haarcascade_frontalface_default.xml")
 capture = cv2.VideoCapture(0)
+t_end = time.time() + 5
 
-while (True):
+
+faces_detected = 0
+total_iterations = 0
+fid = 0
+input()
+while (time.time() < t_end):
     ret, cframe = capture.read()
     gframe = cv2.cvtColor(cframe, cv2.COLOR_BGR2GRAY)
   
@@ -34,8 +42,14 @@ while (True):
     show_faces()
 
     cv2.imshow("Face Tracker", cframe)
+    if (len(faces) != 0):
+            faces_detected = faces_detected + 1
+    total_iterations = total_iterations + 1
     if (cv2.waitKey(20) & 0xFF == ord('q')):
         break
 
+success_rate = (faces_detected / total_iterations) * 100
+sys.stdout.flush()
+print("Efficiency rate:", success_rate)
 capture.release()
 cv2.destroyAllWindows()
